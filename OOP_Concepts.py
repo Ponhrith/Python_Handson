8db8b63e201668a4ec5dbc7ace17bb42b5a6aad9
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-30 20:12:19 +0700

    Constructor

--- OOP_Concepts.py
+++ OOP_Concepts.py
@@ -1,16 +1,19 @@
 class Car:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+    
     def brand(self):
         print("Audi")
         
     def color(self):
         print("Black")
         
-car = Car() #Object
-car.brand() #magic method or dundes
-car.start = "Start"
-car.stop = car.start
+    def move(self):
+        print("Move")
+        
+# A constructor is a function that gets called at the time of creating an object.
+
+car = Car("Start", "Stop")
+car.start = "start"
 print(car.start)
-car2 = Car()
-car2 = Car()
-car2.stop = "Stop"
-print(car2.stop) 
