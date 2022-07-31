a60f3c86f49cb71034dd62777b314cc214a7ea96
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-31 11:42:17 +0700

    Calculating Total and Saved/Not Yet Working

--- Project1/app.py
+++ Project1/app.py
@@ -10,5 +10,12 @@ for row in range(2, sheet.max_row + 1):
     geography = sheet.cell(row, 6).value
     biology = sheet.cell(row, 7).value
     chemistry = sheet.cell(row, 8).value
-    print(physics, maths, history, geography, biology, chemistry)
+    
 
+
+    total_marks = physics + maths + history + geography + biology + chemistry
+    total_marks_cell = sheet.cell(row, 9)
+    total_marks_cell.value = total_marks
+    
+workbook.save("students-result-final.xlsx")
+print("Document Saved Successfully")
