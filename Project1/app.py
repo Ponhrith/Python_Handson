15c63611b8e895b375b4c8fcded4edaafba56b0c
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-31 11:35:34 +0700

    Progress

--- Project1/app.py
+++ Project1/app.py
@@ -2,4 +2,13 @@ import openpyxl as xl
 
 workbook = xl.load_workbook("students-result.xlsx")
 sheet = workbook["Sheet1"]
-print(sheet.cell(4, 4).value)
+
+for row in range(2, sheet.max_row + 1):
+    physics = sheet.cell(row, 3).value
+    maths = sheet.cell(row, 4).value
+    history = sheet.cell(row, 5).value
+    geography = sheet.cell(row, 6).value
+    biology = sheet.cell(row, 7).value
+    chemistry = sheet.cell(row, 8).value
+    print(physics, maths, history, geography, biology, chemistry)
+
