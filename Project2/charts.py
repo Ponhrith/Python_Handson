1581ad2658dfeca7295d2022fea6df3a6b27b93c
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:30:31 +0700

    Color Added

--- Project2/charts.py
+++ Project2/charts.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 months = [1,2,3]
 income1 = [1000, 4000, 10000]
 income2 = [2000, 3000, 4000]
-plt.plot(months, income1, marker = 'o')
-plt.plot(months, income2, marker="o")
+plt.plot(months, income1, marker = 'o', color='r')
+plt.plot(months, income2, marker="o", color='blue')
 plt.title("Income of Company 1 & Company 2\n")
 plt.xlabel("\nMonths")
 plt.ylabel("Income\n")
