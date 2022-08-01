f0496bbc08ea7abbd2af6814516bdf5c1b139ed2
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:52:20 +0700

    Percentage format

--- Project2/PieCharts.py
+++ Project2/PieCharts.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 continents = ["North America", "South America", "Europe", "Asia", "Africa"]
 population = [57900000, 422000000, 741000000, 4463000000, 1216000000]
-plt.pie(population, labels=continents, autopct="%f") #f = fraction
+plt.pie(population, labels=continents, autopct="%1.1f%%") #f = fraction, 1.1 = 1 digit, %% percent 
 plt.legend(continents)
 plt.show()
 
