ca4a965d0e8921815292b1e6ad2f5f1af0684967
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:39:40 +0700

    Title and Labled Added

--- Project2/barCharts.py
+++ Project2/barCharts.py
@@ -3,4 +3,7 @@ from matplotlib import pyplot as plt
 days = [1,2,3,4,5,6,7]
 temperature = [27,34,30,38,25,21,29]
 plt.bar(days, temperature)
+plt.title("Temperature Record of the Last Week\n")
+plt.xlabel("\nDays")
+plt.ylabel("\nTemperature (C)")
 plt.show()
