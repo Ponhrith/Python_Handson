a1be54517133de55297f5021e3dbf4cdcbdb3060
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:29:17 +0700

    Add marker

--- Project2/charts.py
+++ Project2/charts.py
@@ -2,10 +2,12 @@ from matplotlib import pyplot as plt
 
 months = [1,2,3]
 income1 = [1000, 4000, 10000]
-income2 = [3000, 2000, 7000]
-plt.plot(months, income1)
-plt.plot(months, income2)
+income2 = [2000, 3000, 4000]
+plt.plot(months, income1, marker = 'o')
+plt.plot(months, income2, marker="o")
 plt.title("Income of Company 1 & Company 2\n")
 plt.xlabel("\nMonths")
 plt.ylabel("Income\n")
+plt.legend(["Company 1", "Company 2"])
+plt.grid()
 plt.show()
