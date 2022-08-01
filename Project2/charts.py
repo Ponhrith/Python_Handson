1520f198702a928f235a395ce82836abbf8420cd
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:23:50 +0700

    Income 2 Added

--- Project2/charts.py
+++ Project2/charts.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
 
 months = [1,2,3]
-income = [1000, 4000, 10000]
-plt.plot(months, income)
+income1 = [1000, 4000, 10000]
+income2 = [3000, 2000, 7000]
+plt.plot(months, income1)
+plt.plot(months, income2)
 plt.show()
