7a8734df57ab0728dca5474a1c11e26525fa3c31
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:42:24 +0700

    Add night_tem

--- Project2/barCharts.py
+++ Project2/barCharts.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
 
 days = [1,2,3,4,5,6,7]
-temperature = [27,34,30,38,25,21,29]
-plt.bar(days, temperature)
+day_temperature = [27,34,30,38,25,21,29]
+night_tem = [20,27,25,32,21,16,23]
+plt.bar(days, day_temperature)
+plt.bar(days, night_tem)
 plt.title("Temperature Record of the Last Week\n")
 plt.xlabel("\nDays")
 plt.ylabel("\nTemperature (C)")
