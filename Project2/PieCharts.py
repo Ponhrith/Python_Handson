5b5402e3a187afe64db6efb10536c41c7a9bd76c
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:51:28 +0700

    Percentage Added

--- Project2/PieCharts.py
+++ Project2/PieCharts.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 
 continents = ["North America", "South America", "Europe", "Asia", "Africa"]
 population = [57900000, 422000000, 741000000, 4463000000, 1216000000]
-plt.pie(population, labels=continents)
+plt.pie(population, labels=continents, autopct="%f") #f = fraction
 plt.legend(continents)
 plt.show()
+
