1a7e3cbd3f27bc85074f2b93d3903e821055544a
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:53:55 +0700

    Axis-Aligned

--- Project2/PieCharts.py
+++ Project2/PieCharts.py
@@ -4,5 +4,6 @@ continents = ["North America", "South America", "Europe", "Asia", "Africa"]
 population = [57900000, 422000000, 741000000, 4463000000, 1216000000]
 plt.pie(population, labels=continents, autopct="%1.1f%%") #f = fraction, 1.1 = 1 digit, %% percent 
 plt.legend(continents)
+plt.axis("equal") #aligned
 plt.show()
 
