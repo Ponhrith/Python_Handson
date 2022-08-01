6683ed25d53bf5d29346c2f2b86518d1d2e989b5
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:26:15 +0700

    x and y label

--- Project2/charts.py
+++ Project2/charts.py
@@ -5,4 +5,7 @@ income1 = [1000, 4000, 10000]
 income2 = [3000, 2000, 7000]
 plt.plot(months, income1)
 plt.plot(months, income2)
+plt.title("Income of Company 1 & Company 2\n")
+plt.xlabel("\nMonths")
+plt.ylabel("Income\n")
 plt.show()
