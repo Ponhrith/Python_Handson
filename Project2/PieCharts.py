c7df298544f8afcfe5cff0ef50dd12c6cd16bef8
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:57:01 +0700

    Explode-Isolate

--- Project2/PieCharts.py
+++ Project2/PieCharts.py
@@ -2,8 +2,9 @@ from matplotlib import pyplot as plt
 
 continents = ["North America", "South America", "Europe", "Asia", "Africa"]
 population = [57900000, 422000000, 741000000, 4463000000, 1216000000]
-plt.pie(population, labels=continents, autopct="%1.1f%%") #f = fraction, 1.1 = 1 digit, %% percent 
-plt.pie(population, labels=continents, autopct="%1.1f%%", startangle=45) #change position
+explode = (0,0,0.1,0,0) #isolate
+plt.pie(population, labels=continents, autopct="%1.1f%%", explode = explode) #f = fraction, 1.1 = 1 digit, %% percent 
+#plt.pie(population, labels=continents, autopct="%1.1f%%", startangle=45) #change position
 plt.legend(continents)
 plt.axis("equal") #aligned
 plt.show()
