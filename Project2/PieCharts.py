36c2cefd5aca7c35901853976cec9a12012d7aff
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:50:28 +0700

    Label Added

--- Project2/PieCharts.py
+++ Project2/PieCharts.py
@@ -2,6 +2,6 @@ from matplotlib import pyplot as plt
 
 continents = ["North America", "South America", "Europe", "Asia", "Africa"]
 population = [57900000, 422000000, 741000000, 4463000000, 1216000000]
-plt.pie(population)
+plt.pie(population, labels=continents)
 plt.legend(continents)
 plt.show()
