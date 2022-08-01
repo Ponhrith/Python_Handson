6e6617cc1d7fa6a4aa03f98ba6cdfab58377177f
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-01 22:55:04 +0700

    start angle

--- Project2/PieCharts.py
+++ Project2/PieCharts.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 continents = ["North America", "South America", "Europe", "Asia", "Africa"]
 population = [57900000, 422000000, 741000000, 4463000000, 1216000000]
 plt.pie(population, labels=continents, autopct="%1.1f%%") #f = fraction, 1.1 = 1 digit, %% percent 
+plt.pie(population, labels=continents, autopct="%1.1f%%", startangle=45) #change position
 plt.legend(continents)
 plt.axis("equal") #aligned
 plt.show()
